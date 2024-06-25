885b61af0ca207af3f50c5b0cccf0bc5efdfb69c
Nahyan Sharvin <nskonnalath@gmail.com>
2024-06-26 00:13:52 +0530

    test: add shadcn button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+// import { Button } from './components/ui/button'
+import { Button } from '@/components/ui/button'
+
 function App() {
 
   return (
     <>
       <h1 className="text-red-600">blog App</h1>
+      <Button>Click me</Button>
     </>
   )
 }
