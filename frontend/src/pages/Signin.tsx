550899c94609115c7ab6404799d561ef65d89ea5
Nahyan Sharvin <nskonnalath@gmail.com>
2024-06-26 01:02:00 +0530

    feat: add signin page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -2,25 +2,19 @@ import { Input } from "Components/ui/input"
 import { Button } from "Components/ui/button"
 import Quote from "../assets/quote.svg"
 
-export function Signup() {
+export function Signin() {
     return (
         <div className="flex min-h-screen">
             <div className="flex flex-1">
                 <div className="flex flex-col justify-center w-full max-w-md p-8 mx-auto">
-                    <h1 className="mb-2 text-3xl font-bold">Create an account</h1>
+                    <h1 className="mb-2 text-3xl font-bold">Login to your account</h1>
                     <p className="mb-6 text-gray-500">
-                        Already have an account?{" "}
+                        Dont have an account?{" "}
                         <a href="#" className="text-blue-500">
-                            Login
+                            SignUp
                         </a>
                     </p>
                     <form className="space-y-4">
-                        <div>
-                            <label htmlFor="username" className="block mb-1 font-medium">
-                                Username
-                            </label>
-                            <Input id="username" placeholder="Enter your username" />
-                        </div>
                         <div>
                             <label htmlFor="email" className="block mb-1 font-medium">
                                 Email
@@ -34,7 +28,7 @@ export function Signup() {
                             <Input id="password" placeholder="Enter your password" type="password" />
                         </div>
                         <Button className="w-full mt-4" variant="default">
-                            Sign Up
+                            Sign In
                         </Button>
                     </form>
                 </div>
@@ -44,9 +38,9 @@ export function Signup() {
                 <div className="max-w-md p-8 relative">
                     <img src={Quote} alt="Quote" className="w-20 h-20 absolute -left-12 -top-4" />
                     <blockquote className="text-3xl font-semibold">
-                        Don’t focus on having a great blog. Focus on producing a blog that’s great for your readers.
+                    Blogs are whatever we make them. Defining ‘Blog’ is a fool’s errand.
                     </blockquote>
-                    <p className="mt-3 text-slate-400 text-lg">- Brian Clark</p>
+                    <p className="mt-3 text-slate-400 text-lg">- Michael Conniff</p>
                 </div>
             </div>
         </div>
