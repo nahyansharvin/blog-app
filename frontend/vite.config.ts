863e4247657c650c6cf3dafe7b9f7e03fb625894
Nahyan Sharvin <nskonnalath@gmail.com>
2024-06-26 00:17:45 +0530

    config: add Component import alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      Components: path.resolve(__dirname, "./src/components"),
     },
   },
 })
