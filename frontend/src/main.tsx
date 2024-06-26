c4deb7dfb2bc86a407e483918f1c535fc9359e67
Nahyan Sharvin <nskonnalath@gmail.com>
2024-06-26 14:58:45 +0530

    feat: add latest react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import { HomePage, Signup, Signin, BlogView, ErrorPage } from 'Pages/index'
+import AuthLayout from './Layout/AuthLayout'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AuthLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      
+    ]
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
