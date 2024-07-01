1259a91ca93789c30294c40b9c3c6beebe9d0c05
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-01 14:52:43 +0530

    feat: add cors header

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,13 @@ import { authMiddleware } from './middlewares/auth'
 import { HonoBindings } from './config/types'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
+import { cors } from 'hono/cors'
 
 const app = new Hono<HonoBindings>().basePath('/api/v1')
 
 //middlewares
 app.use('*', prismaMiddleware )
+app.use('*', cors())
 
 app.route('/user', userRouter)
 app.route('/blog', blogRouter)
