c32340a1a674302e0085f7df908ed0c394406beb
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-02 16:53:03 +0530

    chore: remove async await from service api declaration

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -4,10 +4,10 @@ export const signIn = ({ email, password }: any) => {
     return BackendService.post("/auth/signin", { email, password });
 }
 
-export const signUp = async ({ username, email, password }: any) => {
-    return await BackendService.post("/auth/signup", { username, email, password });
+export const signUp = ({ username, email, password }: any) => {
+    return BackendService.post("/auth/signup", { username, email, password });
 }
 
-export const signOut = async () => {
-    return await BackendService.post("/auth/signout");
+export const signOut = () => {
+    return BackendService.post("/auth/signout");
 }
