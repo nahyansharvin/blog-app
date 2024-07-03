775e6d425a5546dd3c9e5d7928ba1e1f377d49c6
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-03 09:30:44 +0530

    feat: add back button in blog view page

--- frontend/src/pages/BlogView.tsx
+++ frontend/src/pages/BlogView.tsx
@@ -1,14 +1,16 @@
+import { Button } from "@/components/ui/button"
 import { handleApiError } from "@/lib/handleApiError"
 import { SingleBlogResponse } from "@/lib/types"
 import { getBlog } from "@/services/BlogService"
 import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export function BlogView() {
+    const navigate = useNavigate()
     const location = useLocation()
     const id = location.pathname.split("/")[2]
-    const [blog, setBlog] = useState<SingleBlogResponse|null>(null)
-    
+    const [blog, setBlog] = useState<SingleBlogResponse | null>(null)
+
     const fetchBlog = async () => {
         try {
             const response = await getBlog(id)
@@ -19,13 +21,15 @@ export function BlogView() {
         }
     }
 
-    useEffect(() => {        
+    useEffect(() => {
         fetchBlog()
     }, [])
 
     return (
-        <div className="px-4 py-6 md:px-6 md:py-12 lg:py-16">
-            <article className="prose prose-gray max-w-3xl mx-auto dark:prose-invert">
+        <div className="px-4 py-6 md:px-6 md:py-12 lg:py-15">
+            <article className="prose prose-gray max-w-4xl mx-auto dark:prose-invert relative">
+                {/* Back Button */}
+                <Button variant="link" className="absolute -top-11 -left-4" onClick={() => navigate("/")}>Back</Button>
                 <div className="space-y-4 not-prose">
                     <h1 className="text-4xl font-extrabold font-heading tracking-tight lg:text-5xl">
                         {blog?.blog.title}
