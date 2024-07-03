82a1c0e97ea5195552eca3a81f169dc19d89d45a
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-03 08:42:18 +0530

    feat: change router config to App.js

---
+++ frontend/src/App.tsx
@@ -0,0 +1,58 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { HomePage, Signup, Signin, BlogView, ErrorPage, CreateBlog } from 'Pages/index'
+import AuthLayout from '@/Layout/AuthLayout'
+import PublicLayout from '@/Layout/PublicLayout'
+import { authAtom } from '@/store/authAtom'
+import { useRecoilValue } from 'recoil'
+
+const authRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AuthLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <HomePage />,
+            },
+            {
+                path: '/blog/:id',
+                element: <BlogView />,
+            },
+            {
+                path: "/write",
+                element: <CreateBlog />
+            }
+
+        ]
+    }
+])
+
+const unAuthRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <PublicLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <HomePage />,
+            }
+        ]
+    },
+    {
+        path: '/signin',
+        element: <Signin />,
+    },
+    {
+        path: '/signup',
+        element: <Signup />,
+    }
+])
+
+function App() {
+    const { isAuthenticated } = useRecoilValue(authAtom)
+    return <RouterProvider router={isAuthenticated ? authRouter : unAuthRouter} />
+}
+
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,50 +1,15 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
 import { RecoilRoot } from 'recoil'
-
-import { HomePage, Signup, Signin, BlogView, ErrorPage, CreateBlog } from 'Pages/index'
-import AuthLayout from './Layout/AuthLayout'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <AuthLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/blog/:id',
-        element: <BlogView />,
-      },
-      {
-        path: "/write",
-        element: <CreateBlog />
-      }
-
-    ]
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/signin',
-    element: <Signin />,
-  }
-])
-
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
       <Toaster />
-      <RouterProvider router={router} />
+      <App />
     </RecoilRoot>
   </React.StrictMode>,
 )
