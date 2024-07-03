7cb1903d6a153ea1efea0c77386a0222a7eccd45
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-03 13:01:47 +0530

    fix: change env variable name

--- frontend/src/services/BackendService.ts
+++ frontend/src/services/BackendService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = import.meta.env.VITE_BASE_URL
+const baseUrl = import.meta.env.VITE_API_BASE_URL
 
 const axiosInstance = axios.create({
     baseURL: `${baseUrl}/api/v1`,
