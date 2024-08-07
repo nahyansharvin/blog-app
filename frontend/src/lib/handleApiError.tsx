c51581e51967cf59715b9af28ec12ba2d0a852a4
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-07 22:05:10 +0530

    feat: handle session expiry

--- frontend/src/lib/handleApiError.tsx
+++ frontend/src/lib/handleApiError.tsx
@@ -1,13 +1,19 @@
 import { Error } from "@/lib/toast"
+import { authAtom } from '@/store/authAtom'
+import { resetRecoil } from "recoil-nexus"
 
 export const handleApiError = (error: any) => {
     if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log("Error response >> ", error.response.data)
-        console.log("Error response >> ", error.response.status)
-        console.log("Error response >> ", error.response.headers)
-        return Error(error.response.data.message)
+        if (error.response.status === 401) {
+            resetRecoil(authAtom)
+            return Error("Session expired. Please login again")
+        }
+        // console.log("Error response >> ", error.response.data)
+        // console.log("Error response >> ", error.response.status)
+        // console.log("Error response >> ", error.response.headers)
+        return Error("Backend adich poyi guys!")
     } else if (error.request) {
         // The request was made but no response was received
         console.log("Error request >> ", error.request)
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import { Toaster } from 'react-hot-toast'
 import { RecoilRoot } from 'recoil'
 import App from './App'
+import RecoilNexus from 'recoil-nexus'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
+      <RecoilNexus />
       <Toaster />
       <App />
     </RecoilRoot>
